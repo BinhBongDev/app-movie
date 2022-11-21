694dc811a030d67c546878adf819118a14ad358c
BinhBongDev <hoangvanbinht37@gmail.com>
2022-11-21 18:50:38 +0700

    8. add btn, show UI

--- src/components/HeroSlide/index.js
+++ src/components/HeroSlide/index.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import {Swiper, SwiperSlide} from 'swiper/react'
-import 'swiper/css';
+import {useNavigate} from 'react-router-dom'
 
+import 'swiper/css';
 import './style.scss'
 
-import tmdbApi, {category, movieType} from '../../api/tmdbApi'
+import tmdbApi, {movieType} from '../../api/tmdbApi'
 import apiConfig from '../../api/apiConfig'
+import Button, {OutlineButton} from '../Button';
 
 
 const HeroSlide = () => {
@@ -36,7 +38,10 @@ const HeroSlide = () => {
                 movieItems.map(movie => (
                     <SwiperSlide key={movie.id}>
                         {({isActive}) => (
-                            <img src={apiConfig.originalImage(movie.backdrop_path)} alt={movie.title} />
+                            <HeroSlideItem
+                                item={movie}
+                                className={`${isActive ? 'active' : ''}`}
+                            />
                         )}
                     </SwiperSlide>
                 ))
@@ -45,6 +50,41 @@ const HeroSlide = () => {
         </Swiper>
     </div>
   )
+}
+
+const HeroSlideItem = props => {
+    let navigate = useNavigate()
+    const item = props.item
+
+    const background = apiConfig.originalImage(item.backdrop_path ? item.backdrop_path : item.poster_path)
+
+    return (
+        <div
+            className={`hero-slide__item ${props.className}`}
+            style = {{backgroundImage: `url(${background})`}}
+        >
+            <div className='hero-slide__item__content container'>
+                <div className='hero-slide__item__content__info'>
+                    <h2 className='title'>{item.title}</h2>
+                    <p className='overview'>{item.overview}</p>
+                    <div className='btns'>
+                        <Button
+                            onClick={() => navigate(`/movie/${item.id}`)}>
+                            Watch Now
+                        </Button>
+                        <OutlineButton onClick = {() => console.log(`Trailer ID: ${item.id}`)}>
+                            Watch Trailer
+                        </OutlineButton>
+                    </div>
+                </div>
+                <div className='hero-slide__item__content__poster'>
+                    <img src={apiConfig.w500Image(item.poster_path)} alt={item.title} />
+                </div>
+
+            </div>
+
+        </div>
+    )
 }
 
 export default HeroSlide
