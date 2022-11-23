dcd987849c5eb9b059de428610c1aab52820a722
BinhBongDev <hoangvanbinht37@gmail.com>
2022-11-23 19:53:28 +0700

    12. add cp footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.scss';
 
 import Header from './components/Header';
 
+import Footer from './components/Footer'
+
 import Routess from './configRoute/Routes';
 
 function App() {
@@ -13,7 +15,7 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routess />
-
+      <Footer />
     </BrowserRouter>
   );
 }
